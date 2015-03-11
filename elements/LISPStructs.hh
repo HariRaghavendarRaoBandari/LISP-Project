89937cbc31087e20d1c63294d7806197e91c1c91
thomas lefeuvre <thomas.lefeuvre@ymail.com>
2015-03-11 16:52:34 +0100

    edit de LISPStruct afin que tout compile (record/locator en doublon)

--- elements/LISPStructs.hh
+++ elements/LISPStructs.hh
@@ -14,6 +14,15 @@
 #define LISP_H_TYPE_8 8	// Encapsulated Control Message
 
 /*
+ * Values for the Key_ID field.
+ */
+
+
+#define KEY_ID_NONE                     0
+#define KEY_ID_HMAC_SHA_1_96    1 // defined in [RFC2404]
+#define KEY_ID_HMAC_SHA_256_128 2 // defined in [RFC4868]
+
+/*
  * Values for AFI (Address Family Identifier) fields.
  * @see https://www.iana.org/assignments/address-family-numbers/address-family-numbers.xhtml
  */
@@ -49,10 +58,54 @@ struct LISPMapRegisterOuterHeader {
 #endif
 } CLICK_SIZE_PACKED_ATTRIBUTE;
 
+struct LISPMapRegisterInnerHeader {
+    unsigned int Key_Id : 16; //  16 bits but we use a mask here
+    unsigned int Authentication_Data_Length : 16; // number of bytes for Authentication_Data field -- 16 bits but we use a mask here 
+    unsigned int Authentication_Data : 32;  // uncomment and modify if needed
+}CLICK_SIZE_PACKED_ATTRIBUTE;
+
+struct LISPLocatorHeader {
+#if CLICK_BYTE_ORDER == CLICK_LITTLE_ENDIAN     
+     unsigned int Priority : 8;
+     unsigned int Weight : 8;
+     unsigned int M_Priority : 8;
+     unsigned int M_Weight : 8;
+     unsigned int Unused_Flags : 13;
+     unsigned int L : 1;
+     unsigned int P : 1;
+     unsigned int R : 1;
+     unsigned int Loc_AFI : 16;
+     unsigned int Locator : 32;
+#else
+#error "Only little endian is supported"
+#endif
+}CLICK_SIZE_PACKED_ATTRIBUTE;
+
+struct LISPRecordLocatorHeader{
+#if CLICK_BYTE_ORDER == CLICK_LITTLE_ENDIAN	
+     unsigned int Record_TTL : 32;
+     unsigned int Locator_count : 8;
+     unsigned int EID_mask_len : 8;
+     unsigned int Rsvd1 : 4;
+     unsigned int A : 1;
+     unsigned int ACT : 3;
+     unsigned int Reserved: 8;
+     unsigned int Rsvd2 : 4;
+     unsigned int Map_Version_number : 12;
+     unsigned int EID_Prefix_AFI : 16;
+     unsigned int EID_Prefix : 32; //32 bits (4 octets) pour ipv4, 128 bits (16 octets) pour ipv6
+     struct LISPLocatorHeader loc;
+#else
+#error "Only little endian is supported"
+#endif
+}CLICK_SIZE_PACKED_ATTRIBUTE;
+
 struct LISPMapRegister {
 	struct LISPMapRegisterOuterHeader header;
 	uint32_t nonce1;
 	uint32_t nonce2;
+	struct LISPMapRegisterInnerHeader headerInner;
+	struct LISPRecordLocatorHeader recLoc;
 } CLICK_SIZE_PACKED_ATTRIBUTE;
 
 /*
@@ -247,7 +300,7 @@ struct LISPMapReplyLoc {
 
 #elif CLICK_BYTE_ORDER == CLICK_BIG_ENDIAN
 
-	unsigned int UnusedFlags2:	5;
+	unsigned int UnusedFlags2 	:	5;
 	unsigned int L:			1;
 	unsigned int p:			1;
 	unsigned int R:			1;
