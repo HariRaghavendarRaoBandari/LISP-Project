6cdd409149f57e566c5623f40e9c8f4410242f2d
Thibault Guittet <guittet.thibault@gmail.com>
2015-03-17 19:33:06 +0100

    Ajout d'un cache pour les EIDs de destination.
    Chaque nouveau EID est ajouté dans le cache avec un TTL maximum.
    Chaque seconde, le TTL de tous les EIDs du cache sont décrémentés.
    Quand un EID expire (TTL < 0) il est supprimé du cache et de la DB.
    Si un EID déjà dans le cache est rencontré à nouveau, son TTL est remis au
    maximum.

--- elements/LISPResolv.cc
+++ elements/LISPResolv.cc
@@ -2,20 +2,62 @@
 #include <clicknet/ip.h>
 #include <click/glue.hh>
 #include <click/ipaddress.hh>
+#include <click/args.hh>
 #include "LISPResolv.hh"
 #include "LISPDB.hh"
 
 CLICK_DECLS
 
-LISPResolv::LISPResolv() { }
+LISPResolv::LISPResolv() : _timer((Element *) this) {
+	_max_cache_ttl = 5;
+}
 
 LISPResolv::~LISPResolv() { }
 
+int LISPResolv::configure(Vector<String> &conf, ErrorHandler *errh) {
+	if (Args(conf, this, errh).read("MAXTTL", _max_cache_ttl).complete() < 0)
+		return -1;
+
+	return 0;
+}
+
+int LISPResolv::initialize(ErrorHandler *e) {
+	_timer.initialize((Element *) this);
+	_timer.schedule_now();
+
+	return 0;
+}
+
+void LISPResolv::run_timer(Timer *t) {
+	assert(&_timer == t);
+	// We need to reschedule _before_ checking if _eid_cache is emtpy
+	_timer.reschedule_after_sec(1);
+
+	if (_eid_cache.empty())
+		return;
+
+	click_chatter("@{%s} EID cache:\n", __PRETTY_FUNCTION__);
+
+	// Print & modify the cache
+	for (HashTable<uint32_t, int>::iterator it = _eid_cache.begin(); it ; it++) {
+		_eid_cache[it.key()] -= 1;
+		click_chatter("\t%s -> %d\n", IPAddress(it.key()).s().c_str(), it.value());
+
+		if (_eid_cache[it.key()] < 0) {
+			click_chatter("\t\trecord expired\n");
+			eraseEID(it.key());
+			it = _eid_cache.erase(it);
+		}
+	}
+}
+
 Packet *LISPResolv::simple_action(Packet *p) {
 	click_ip *ip_oh = (click_ip *) p->data();
+	// This is the destination EID
+	uint32_t eid = p->dst_ip_anno();
 
 	// We search for the RLOC associated with the destination EID
-	IPAddress a = IPAddress(getRLOCFromEID(p->dst_ip_anno()));
+	IPAddress a = IPAddress(getRLOCFromEID(eid));
 
 	if (a.empty()) {
 		click_chatter("@{%s}: failed resolv -> output 1\n", __PRETTY_FUNCTION__);
@@ -32,6 +74,7 @@ Packet *LISPResolv::simple_action(Packet *p) {
 
 	p->set_dst_ip_anno(a);
 	ip_oh->ip_dst = a;
+	_eid_cache[eid] = this->_max_cache_ttl; // reset the TTL for the destination EID
 
 	return p;
 }
--- elements/LISPResolv.hh
+++ elements/LISPResolv.hh
@@ -2,6 +2,8 @@
 #define CLICK_LISPRESOLV_HH
 
 #include <click/element.hh>
+#include <click/timer.hh>
+#include <click/hashtable.hh>
 
 CLICK_DECLS
 
@@ -29,10 +31,20 @@ The dst_ip_anno annotation _must_ be set before calling this element.
 
 LISPDB is used for the EID -> RLOC mapping.
 
+This element has as expiration table for destination EIDs. If an EID expires it
+is removed from the Database. This method is used to re-request a RLOC for the
+destination EID, it's usefull if the destination move. The max TTL is 5 by
+default but can be configured with the MAXTTL keyword.
+
 =a LISPDB
 */
 
 class LISPResolv : public Element {
+	HashTable<uint32_t, int> _eid_cache;
+	Timer _timer;
+	int _max_cache_ttl;
+
+	void run_timer(Timer *t);
 public:
 	LISPResolv() CLICK_COLD;
 	~LISPResolv() CLICK_COLD;
@@ -42,6 +54,8 @@ public:
 	const char *processing() const { return PROCESSING_A_AH; }
 
 	Packet *simple_action(Packet *);
+	int initialize(ErrorHandler *);
+	int configure(Vector<String> &conf, ErrorHandler *errh);
 };
 
 CLICK_ENDDECLS
