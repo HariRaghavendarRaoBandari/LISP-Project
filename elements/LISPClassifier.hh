6a15db0a01cee43497d1fcef3fe2de08553067b6
Kévin Pensec <kevin.pensec@gmail.com>
2015-04-01 18:24:05 +0200

    Fix classifier

---
+++ elements/LISPClassifier.hh
@@ -0,0 +1,28 @@
+#ifndef CLICK_LISPCLASSIFIER_HH
+#define CLICK_LISPCLASSIFIER_HH
+
+#include <click/element.hh>
+#include <clicknet/udp.h>
+#include <clicknet/ether.h>
+#include <click/args.hh>
+
+#define LISP_CL_REGISTER 0
+#define LISP_CL_REQUEST	 1
+#define LISP_CL_INVALID	 2
+
+CLICK_DECLS
+
+class LISPClassifier: public Element {
+public:
+	LISPClassifier() CLICK_COLD;
+	~LISPClassifier() CLICK_COLD;
+
+	const char *class_name() const { return "LISPClassifier"; }
+	const char *port_count() const { return "1/3"; }
+	const char *processing() const { return AGNOSTIC; }
+
+	Packet* simple_action(Packet*);
+};
+
+CLICK_ENDDECLS
+#endif
